ff9bebc5f7c90cf90a77b36ff0c15940fa4a16e8
prj <prjsupa@gmail.com>
2025-08-03 16:37:36 +0000

    Uncaught Error: Invalid src prop (https://jqdbhsicpfdpzifphdft.supabase.

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'jqdbhsicpfdpzifphdft.supabase.co',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
